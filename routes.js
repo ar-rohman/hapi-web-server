837c7a9e9aadfa1f5db039751ca0fd64d3c0216c
Rohman <rohmanpendo@gmail.com>
2022-12-24 17:01:00 +0700

    feat: Path Parameter

--- routes.js
+++ routes.js
@@ -2,35 +2,43 @@ const routes = [
   {
     method: 'GET',
     path: '/',
-    handler: (response, h) => {
+    handler: (request, h) => {
       return 'Homepage';
     },
   },
   {
     method: '*',
     path: '/',
-    handler: (response, h) => {
+    handler: (request, h) => {
       return 'Halaman tidak dapat diakses dengan method tersebut';
     },
   },
   {
     method: 'GET',
     path: '/about',
-    handler: (response, h) => {
+    handler: (request, h) => {
       return 'About page';
     },
   },
   {
     method: '*',
     path: '/about',
-    handler: (response, h) => {
+    handler: (request, h) => {
       return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = 'Stranger' } = request.params;
+      return `Hello, ${name}!`;
     },
   },
   {
     method: '*',
     path: '/{any*}',
-    handler: (response, h) => {
+    handler: (request, h) => {
       return 'Halaman tidak ditemukan';
     },
   },
